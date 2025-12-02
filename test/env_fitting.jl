fb8dad22312ce167ecac0990dbf6c0664933b896
cailixun <cailixun23@gmail.com>
2025-12-02 13:13:19 +0800

    fix test

--- test/env_fitting.jl
+++ test/env_fitting.jl
@@ -2,9 +2,9 @@
     using HierarchicalEOM
 
     Γ = 1.0
-    W = 0.1
-    μ = 1.0ß
-    T = 1
+    W = 10
+    μ = 1.0
+    T = 0.01
     tlist = 0:0.01:10
 
     pade_auto, info = auto_Fermion_Lorentz_Pade(sigmax(), Γ, μ, W, T, true; target_nrmse = 1e-4, Nmax = 50)
@@ -24,14 +24,14 @@ end
 
     Γ = 1.0
     W = 0.1
-    μ = 1.0ß
+    μ = 1.0
     T = 1
     tlist = 0:0.01:10
 
     mats_auto, info = auto_Fermion_Matsubara(sigmax(), Γ, μ, W, T, true; target_nrmse = 1e-4, Nmax = 50)
     C1p, C1m = correlation_function(mats_auto, tlist)
 
-    mats_std = Fermion_Matsubara(sigmax(), Γ, μ, W, T, info.N)
+    mats_std = Fermion_Lorentz_Matsubara(sigmax(), Γ, μ, W, T, info.N)
     C2p, C2m = correlation_function(mats_std, tlist)
 
     @test all(isapprox.(real.(C1p), real.(C2p); atol = 1e-3))
