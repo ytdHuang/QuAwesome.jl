77d7e7c597c37eb0d1c57ccf5c522b8820b763c5
cailixun <cailixun23@gmail.com>
2025-12-02 12:14:38 +0800

    add Matsubara test

--- test/env_fitting.jl
+++ test/env_fitting.jl
@@ -1,4 +1,4 @@
-@testitem "auto_Fermion" begin
+@testitem "auto_Fermion_Pade" begin
     using HierarchicalEOM
 
     Γ = 1.0
@@ -12,6 +12,27 @@
 
     pade_std = Fermion_Lorentz_Pade(sigmax(), Γ, μ, W, T, info.N)
     C2p, C2m = correlation_function(pade_std, tlist)
+
+    @test all(isapprox.(real.(C1p), real.(C2p); atol = 1e-3))
+    @test all(isapprox.(imag.(C1p), imag.(C2p); atol = 1e-3))
+    @test all(isapprox.(real.(C1m), real.(C2m); atol = 1e-3))
+    @test all(isapprox.(imag.(C1m), imag.(C2m); atol = 1e-3))
+end
+
+@testitem "auto_Fermion_Matsubara" begin
+    using HierarchicalEOM
+
+    Γ = 1.0
+    W = 0.1
+    μ = 1.0ß
+    T = 1
+    tlist = 0:0.01:10
+
+    mats_auto, info = auto_Fermion_Matsubara(sigmax(), Γ, μ, W, T, true; target_nrmse = 1e-4, Nmax = 50)
+    C1p, C1m = correlation_function(mats_auto, tlist)
+
+    mats_std = Fermion_Matsubara(sigmax(), Γ, μ, W, T, info.N)
+    C2p, C2m = correlation_function(mats_std, tlist)
 
     @test all(isapprox.(real.(C1p), real.(C2p); atol = 1e-3))
     @test all(isapprox.(imag.(C1p), imag.(C2p); atol = 1e-3))
